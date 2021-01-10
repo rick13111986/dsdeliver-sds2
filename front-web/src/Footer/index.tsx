426c4834e4b1ca61ad4dcebfc4ec2d89d4a60976
R PA <jr.lazfer@gmail.com>
2021-01-10 19:21:29 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,27 @@
+import './styles.css';
+import { ReactComponent as YouTubeIcon} from './youtube.svg';
+import { ReactComponent as LinkedinIcon} from './linkedin.svg';
+import { ReactComponent as InstagramIcon} from './instagram.svg';
+
+function Footer(){
+    return(
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+            <div className="footer-icons">
+                <a href="https://www.youtube.com/c/DevSuperior" target="_new">
+                    <YouTubeIcon />
+                </a>
+                
+                <a href="https://linkedin.com/school/DevSuperior/">
+                    <LinkedinIcon />
+                </a>
+
+                <a href="https://www.instagram.com/DevSuperior.ig">
+                    <InstagramIcon />
+                </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
